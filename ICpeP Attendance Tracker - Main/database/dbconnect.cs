98d9c913fad555e26d9da72ee5152bc714b2461d
ye-long-dragon <bvincelawrence@gmail.com>
2025-09-18 21:40:08 +0800

    Add project files.

---
+++ ICpeP Attendance Tracker - Main/database/dbconnect.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace ICpeP_Attendance_Tracker___Main.database
+{
+    public class dbconnect
+    {
+        public string connectionString = "postgresql://postgres:SYSDEV.MMCM.ICpeP@db.axwduoswuwxdqimchpvs.supabase.co:5432/postgres";
+        private NpgsqlConnection conn;
+
+
+        public void startDBConnection()
+        {
+            conn = new NpgsqlConnection(connectionString);
+            conn.Open();
+            Console.WriteLine("Database connection established successfully.");
+        }
+
+        public void stopDBConnection()
+        {
+            if (conn != null)
+            {
+                conn.Close();
+                conn.Dispose();
+                conn = null;
+                Console.WriteLine("Database connection closed successfully.");
+            }
+        }
+
+
+
+    }
+}
